18afd71b8ea1fcd34e5dc8d00eeaf6638000330a
Hassan Khan <contact@hassankhan.me>
2017-06-30 22:50:33 +0100

    Get 100% code coverage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 var connect = require('react-redux').connect
 var bindActionCreators = require('redux').bindActionCreators
 
+const makeDispatchProps = function(component) {
+  return function(dispatch) {
+    return Object.keys(component.actionProps).reduce(function(result, key) {
+      result[key] = bindActionCreators(component.actionProps[key], dispatch);
+      return result;
+    }, {});
+  }
+}
+
 // can't get any simpler than that, as plain js as possible
 module.exports = function(component) {
-  var dispatchProps = component.dispatchProps;
+  var dispatchProps = component.actionProps
+    ? makeDispatchProps(component)
+    : component.dispatchProps;
 
-  if (component.actionProps) {
-    dispatchProps = function(dispatch) {
-      return Object.keys(component.actionProps).reduce(function(result, key) {
-        result[key] = bindActionCreators(component.actionProps[key], dispatch);
-        return result;
-      }, {});
-    };
-  }
   return connect(component.stateProps, dispatchProps)(component);
 }
+
+module.exports.makeDispatchProps = makeDispatchProps;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,6 @@
+const connect = require('../src/index');
+const makeDispatchProps = require('../src/index').makeDispatchProps;
+
 const mockComponentStateProps = {
   name : 'MyStatePropsComponent',
   stateProps : {
@@ -16,22 +19,32 @@ const mockComponentActionProps = {
   },
 };
 
-const connect = require('../src/index');
-
 describe('react-redux-classconnect', () => {
 
-  it('wraps a component with stateProps', () => {
-    const actual = connect(mockComponentStateProps);
-    expect(actual.displayName).toBe(`Connect(${mockComponentStateProps.name})`);
-    expect(actual.stateProps).toBe(mockComponentStateProps.stateProps);
+  describe('#connect', () => {
+
+    it('wraps a component with stateProps', () => {
+      const actual = connect(mockComponentStateProps);
+      expect(actual.displayName).toBe(`Connect(${mockComponentStateProps.name})`);
+      expect(actual.stateProps).toBe(mockComponentStateProps.stateProps);
+    });
+
+    it('wraps a component with actionProps', () => {
+      const actual = connect(mockComponentActionProps);
+      expect(actual.displayName).toBe(`Connect(${mockComponentActionProps.name})`);
+      expect(actual.actionProps).toBe(mockComponentActionProps.actionProps);
+    });
   });
 
-  it('wraps a component with actionProps', () => {
-    // This test is NOT correct - we should be checking the component's
-    // dispatchProps
-    const actual = connect(mockComponentActionProps);
-    expect(actual.displayName).toBe(`Connect(${mockComponentActionProps.name})`);
-    expect(actual.actionProps).toBe(mockComponentActionProps.actionProps);
-  });
 
+  describe('#makeDispatchProps', () => {
+
+    it('creates a valid `dispatchProps()` function when `actionProps` are specified', () => {
+      const actual = makeDispatchProps(mockComponentActionProps);
+
+      expect(actual()).toHaveProperty("foo");
+      expect(actual()).toHaveProperty("bar");
+      expect(actual()).toHaveProperty("baz");
+    });
+  });
 });
